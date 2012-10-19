e82e071cd95697d8601f5fceb456a141f5c469ad
v-p-b <vpbalint@gmail.com>
2012-10-19 13:59:23 +0200

    Added license. Moved script info to README

---
+++ README.md
@@ -0,0 +1,26 @@
+About
+=====
+
+Enumerating GWT RPC calls from obfuscated cache HTML files.
+
+Might output some gibberish, but hopefully you will get the most important parts of the RPC interface.
+
+"Works-on-my-single-target" version, needs heavy testing.
+
+Usage
+=====
+
+> python S2GWTEnum.py XXXXXXXXXXX.cache.html
+
+Thanks
+======
+
+Many thanks to Gotham Digital Science for their great publications about GWT:
+*   http://blog.gdssecurity.com/labs/2010/7/20/gwtenum-enumerating-gwt-rpc-method-calls.html
+*   http://www.gdssecurity.com/l/t.php
+*   https://www.owasp.org/images/7/77/Attacking_Google_Web_Toolkit.ppt
+
+License
+=======
+
+This script uses MIT license
--- S2GWTEnum.py
+++ S2GWTEnum.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 #
-# Enumerating GWT RPC calls from obfuscated cache HTML files.
-# Might output some gibberish, but hopefully you will get the most important parts of the RPC interface.
+# Copyright (c) 2012 Balint Varga-Perke
 #
-# Many thanks to Gotham Digital Science for their great publications about GWT:
-#     * http://blog.gdssecurity.com/labs/2010/7/20/gwtenum-enumerating-gwt-rpc-method-calls.html
-#     * http://www.gdssecurity.com/l/t.php
-#     * https://www.owasp.org/images/7/77/Attacking_Google_Web_Toolkit.ppt
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
 #
-# Usage: python S2GWTEnum.py <XXXXXXXXXXX.cache.html>
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
 #
-# Contact: vpbalint@silentsignal.hu
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
 
 import re
 import sys
